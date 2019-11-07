ab6daea0755a02d060823fde452676ae632ad17c
lakeryi <994376327@qq.com>
2019-11-07 23:43:32 +0800

    Version 1 Complete

--- LRPJ/LRPJ/urls.py
+++ LRPJ/LRPJ/urls.py
@@ -6,6 +6,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import staticfiles
 
 urlpatterns = [
+    url(r'^choose', view.choose_seat),
     url(r'^logout', view.logout),
     url(r'^login', view.login),
 	url(r'^', view.home),
